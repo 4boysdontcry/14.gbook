a2112c21a841d81e54ebfa65c56788e1c4192cf0
4boysdontcry <hgy02166@gmail.com>
2021-06-02 17:16:47 +0900

    file유무에 따른 분리 완료

--- modules/multer-init.js
+++ modules/multer-init.js
@@ -2,7 +2,7 @@ const moment = require('moment');
 const path = require('path');
 const fs = require('fs-extra');
 const { v4: uuid } = require('uuid');
-const { allowExt } = require('../modules/utils');
+const { allowExt, imgExt } = require('../modules/utils');
 const multer  = require('multer');       // middleware
 
 // storage engine의 destination callback
@@ -40,8 +40,8 @@ const limits = { fileSize: mega * 50 }       // bytes 단위
 function fileFilter (req, file, cb) {
     try{
         let ext = path.extname(file.originalname).substr(1).toLowerCase()		// 확장자명을 file의 origianl name을 통해 가져오고, substr(1)을 통해 1번 자리의 글자부터 가져온다.(확장자의 '.' 을 빼기 위함) 이후 소문자로 바꿔 내가 util에 지정해놓은 ext들의 값과 일치시킨다.
-        if(allowExt.includes(ext)) cb(null, true);      // allowExt에 포함된 확장자인지 확인
-        else cb(null, false);
+        if(imgExt.includes(ext)) cb(null, true);      // allowExt에 포함된 확장자인지 확인
+        else cb(  new error('첨부하신 파일은 업로드가 허용되지 않습니다.'), false );
     }
     catch(err) {
         cb(err)		// error가 발생하면 err을 받아 콜백을 실행한다.
@@ -51,6 +51,9 @@ function fileFilter (req, file, cb) {
 // multer instance 생성
 const upload = multer({ storage, limits, fileFilter });		// 변수 upload에 위의 함수와 변수들을 담는다.
 
-
-
 module.exports = { multer, upload };		// 이 모듈을 내보내서 다른 파일에서 불러다 쓰게 해준다.ㄴ
+
+/* 
+size over => error 발생
+filter => error(x), req.body만 넘어감
+*/
--- routes/gbook-router.js
+++ routes/gbook-router.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const { pool } = require('../modules/mysql-init');
+const { upload } = require('../modules/multer-init');
+const { error } = require('../modules/utils');
+
+
 const ejs = {
     tabTitle: 'Express 방명록',
     pageTitle: 'Express를 활용한 방명록 서비스',
@@ -11,5 +16,21 @@ const ejs = {
 router.get('/', async (req,res,next) => {
     res.render('gbook/gbook', { ...ejs })
 })
+
+router.post('/create', upload.single('upfile'), async (req, res, next) => {
+	try {
+		let { writer, content } = req.body;
+		if(req.file) {
+			let { originalname, filename, size, mimetype } = req.file;
+			res.json({ writer, content, originalname, filename, size, mimetype });
+		}
+		else {
+			res.json({ writer, content });
+		}
+	}
+	catch(err) {
+		next(error(err));
+	}
+});
 
 module.exports = router;
